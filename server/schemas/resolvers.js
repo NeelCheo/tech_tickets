e021da786d648310e93c8bcaef019462237f84e4
alylmeier <alylmeier@gmail.com>
2023-05-04 21:28:45 -0400

    add user route working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,54 @@
 const { Ticket, User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
   Query: {
-    tech: async () => {
-      return Ticket.find({});
+    users: async () => {
+      return User.find().populate('tickets');
     },
-    matchups: async (parent, { _id }) => {
-      const params = _id ? { _id } : {};
-      return User.find(params);
+    user: async (parent, { userName }) => {
+      return User.findOne({ userName }).populate('tickets');
     },
+    tickets: async (parent, { userName }) => {
+      const params = userName ? { userName } : {};
+      return Ticket.find(params);
+    },
+    // ticket: async (parent, { ticketId }) => {
+    //   return Ticket.findOne({ _id: ticketId });
+    // },
   },
   Mutation: {
-    createMatchup: async (parent, args) => {
-      const matchup = await Matchup.create(args);
-      return matchup;
+    addUser: async (parent, { userName, email, password, name, phone }) => {
+      // First we create the user
+      const user = await User.create({ userName, email, password, name, phone  });
+      // To reduce friction for the user, we immediately sign a JSON Web Token and log the user in after they are created
+      const token = signToken(user);
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
+    login: async (parent, { userName, password }) => {
+      // Look up the user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email
+      const user = await User.findOne({ userName });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('No user found with this user name');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+
+      // Return an `Auth` object that consists of the signed token and user's information
+      return { token, user };
     },
   },
 };
