3286c93ccb6cc97b0820d4a6ecfecfac2e4b8159
alylmeier <alylmeier@gmail.com>
2023-05-15 15:10:21 -0400

    more styling

--- client/src/components/pages/Opentix.js
+++ client/src/components/pages/Opentix.js
@@ -17,7 +17,7 @@ const TicketList = () => {
     return (
       <main>
         <div className="flex-row justify-center">
-          <div className="col-12 col-md-8 mb-3">
+          <div className="col-12 col-md-8 mb-3 ">
             {/* If the data is still loading, render a loading message */}
             {loading ? (
               <div>Loading...</div>
